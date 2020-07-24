65685505217e4625609fb3e946f2110eb9d7fad8
schavanpq <suvarna.chavan@proquest.com>
2020-07-24 06:10:43 -0700

    Initial commit

---
+++ src/test/java/testcase/LoginTest.java
@@ -0,0 +1,43 @@
+package testcase;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class LoginTest {
+
+    public static WebDriver driver;
+
+    @BeforeTest
+    public void setUp(){
+        System.setProperty("webdriver.chrome.driver",
+                System.getProperty("user.dir") + "//src//test//resources//chromedriver");
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-extensions");
+        options.addArguments("--disable-infobars");
+        driver = new ChromeDriver(options);
+    }
+
+    @Test
+    public void doLogin(){
+        driver.get("http://google.com");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        //WebDriverWait wait = new WebDriverWait(driver, 5);
+        driver.findElement(By.name("q")).isDisplayed();
+
+    }
+
+    @AfterTest
+    public void tearDown(){
+        driver.quit();
+    }
+}
